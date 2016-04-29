7988044fc0939be8bcfac7e9f22e82011b7a8b84
Antonio Cheltenham <acheltenham@gmail.com>
2016-04-29 16:53:21 -0400

    LCM Script

---
+++ LCM/LCMPushNewWay.ps1
@@ -0,0 +1,23 @@
+﻿[DSCLocalConfigurationManager()]
+Configuration LCMPUSH 
+{	
+	Node $Computername
+	{
+		SEttings
+		{
+			AllowModuleOverwrite = $True
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+			RefreshMode = 'Push'
+            RebootNodeIfNeeded = $True	
+		}
+	}
+}
+
+$Computername = 'localhost'
+
+# Create the Computer.Meta.Mof in folder
+LCMPush -OutputPath c:\DSC\LCM
+
+Set-DscLocalConfigurationManager -Computername $computername -Path c:\DSC\LCM -verbose
+
+
