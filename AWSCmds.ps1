b6a316b697f5d366c1413858dd038d768c5e8327
Antonio Cheltenham <acheltenham@gmail.com>
2016-08-30 20:48:29 -0400

    New Scripts
    These are new

---
+++ AWSCmds.ps1
@@ -0,0 +1,8 @@
+﻿### AWS Set up profile 
+Set-AWSCredentials -AccessKey AKIAJJBTDIIKL67RBY3Q -SecretKey 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 -StoreAs SSO
+## Check list of profiles
+Get-AWSCredentials -ListStoredCredentials
+## Remove Profile
+Clear-AWSCredentials -StoredCredentials (profilename)
+## To use a profile for a sesison 
+Set-AWSCredentials -ProfileName SSO
